777b7332e250d277f2ce43f05d6eaf02008e07fa
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-19 17:40:24 +0900

    querydsl 적용

---
+++ src/main/java/com/example/gb/repository/GuestbookRepository.java
@@ -0,0 +1,8 @@
+package com.example.gb.repository;
+
+import com.example.gb.entity.GuestBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface GuestbookRepository extends JpaRepository<GuestBook, Long>, QuerydslPredicateExecutor<GuestBook> {
+}
