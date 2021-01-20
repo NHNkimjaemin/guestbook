737512e1fbab05248189e6e57875aa5136cc2589
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-20 11:22:17 +0900

    화면 출력 및 css에러 수정

--- src/main/java/com/example/gb/controller/GuestbookController.java
+++ src/main/java/com/example/gb/controller/GuestbookController.java
@@ -1,20 +1,34 @@
 package com.example.gb.controller;
 
 
+import com.example.gb.dto.PageRequestDTO;
+import com.example.gb.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
-    @GetMapping({"/", "/list"})
-    public String list(){
 
-        log.info("list..................");
+    private final GuestbookService service;
 
-        return "/guestbook/list";
+    @GetMapping("/")
+    public String index(){
+
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("list. .. .. . . ... .. . . .. " + pageRequestDTO);
+
+        //이 부분이 타임리프 html에 패러미터를 넘겨주는 부분
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
