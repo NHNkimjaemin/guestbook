58882f416f4c227346bb17b771eb5fbb80a2363a
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-20 13:33:53 +0900

    4.8 완료

--- src/main/java/com/example/gb/controller/GuestbookController.java
+++ src/main/java/com/example/gb/controller/GuestbookController.java
@@ -9,6 +9,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -51,5 +52,14 @@ public class GuestbookController {
 
         // POST 처리 후 리다이렉션
         return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+    public void read(Long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
+        log.info("gno: " + gno);
+
+        GuestbookDTO dto = service.read(gno);
+
+        model.addAttribute("dto", dto);
     }
 }
