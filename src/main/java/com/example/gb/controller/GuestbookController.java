5d1401b90c71d2130b23eaaa644f23a837b898e7
NHNkimjaemin <jaemin.kim@nhn.com>
2021-01-20 12:00:29 +0900

    register page와 등록 완료 모달생성

--- src/main/java/com/example/gb/controller/GuestbookController.java
+++ src/main/java/com/example/gb/controller/GuestbookController.java
@@ -1,6 +1,7 @@
 package com.example.gb.controller;
 
 
+import com.example.gb.dto.GuestbookDTO;
 import com.example.gb.dto.PageRequestDTO;
 import com.example.gb.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -30,5 +33,23 @@ public class GuestbookController {
 
         //이 부분이 타임리프 html에 패러미터를 넘겨주는 부분
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+
+        log.info("dto...", dto);
+
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        // POST 처리 후 리다이렉션
+        return "redirect:/guestbook/list";
     }
 }
